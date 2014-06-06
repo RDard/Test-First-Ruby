04e020d7cd3bcf27e2e9cf8153b2da46033ce9b3
RDard <robdardenelle@gmail.com>
2014-06-05 17:16:53 -0700

    5 out of 7 done

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,13 +1,22 @@
 def translate original_word
-	first_vowel_pos = 0
+    first_vowel_pos = 0
 	translated_word = ""
-
+    puts original_word.index("qu")
+    
 	if original_word.include?(' ')
 		return original_word.split.map{ |x| translate(x) }.join ' '
 	end
 
 	else 
-		first_vowel_pos = original_word.index(/[aeiou]/)
+        if original_word.include?("qu")
+            if original_word.index("qu") < original_word.index(/[aeiou]/)
+		     first_vowel_pos = original_word.index("qu") + 2
+            end
+        
+        else
+            first_vowel_pos = original_word.index(/[aeiou]/)
+        end
+        
 		translated_word = original_word[first_vowel_pos, original_word.length] + original_word[0, first_vowel_pos] + "ay"
 	
 		return translated_word	
